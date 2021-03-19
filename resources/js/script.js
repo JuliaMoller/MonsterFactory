0539b271cde26adaca398aeb1a0c68e76f7b50c0
JuliaMoller <juliadmoller@gmail.com>
2021-03-19 14:56:07 +0000

    to change paws

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,15 +5,9 @@ function changeCanvasColor () {
     document.getElementById("monsterCanvas").style.backgroundColor = color;
 }
 
-// to change monster's paws
-function changePaws () {
-    let paws = document.getElementById("monsterPaws").value;
-    document.getElementsByClassName("paw")[0].setAttribute("data", "svg/monster/paws2.svg");
-}
-
 // to change monster's body color
 function changeMonsterBody () {
-     document.getElementById("monsterBody").style.filter = "saturate(100%) hue-rotate(5deg)";
+     document.getElementById("monsterBody").style.filter = "saturate(85%) hue-rotate(250deg)";
 }
 
 // to change monster's mounth
@@ -23,6 +17,17 @@ var mouths = [
     "resources/svg/monster/mouth3.svg"
 ]
 function changeMouth() {
-    var m = document.getElementById("mouths").value;
-    document.getElementsByClassName("mouth")[0].setAttribute("data", mouths[m]);
+    var i = document.getElementById("mouths").value;
+    document.getElementsByClassName("mouth")[0].setAttribute("data", mouths[i]);
+}
+
+// to change monster's paws
+var paws = [  
+    "resources/svg/monster/paws1.svg", 
+    "resources/svg/monster/paws2.svg", 
+    "resources/svg/monster/paws3.svg"
+]
+function changePaws () {
+    let i = document.getElementById("paws").value;
+    document.getElementsByClassName("paw")[0].setAttribute("data", paws[i]);
 }
