e5532fc7e140806bab3d569aa73a71c97271cace
JuliaMoller <juliadmoller@gmail.com>
2021-03-28 17:56:34 +0100

    Added FileSaver.js and included "save as"

--- resources/js/print.js
+++ resources/js/print.js
@@ -1,11 +1,13 @@
 function printMonster (){
     // dom to image
+
     setTimeout(() => {
-        domtoimage.toPng(monsterCanvas)
-        .then(function (dataUrl) {
-            var img = new Image();
-            img.src = dataUrl;
-            document.body.appendChild(img);
+        domtoimage.toBlob(monsterCanvas)
+        .then(function (blob) {
+            saveAs(blob, "monster.png")
+            // var img = new Image();
+            // img.src = dataUrl;
+            // document.body.appendChild(img);
         })
         .catch(function (error) {
             console.error('oops, something went wrong!', error);
