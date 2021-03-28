a6db6853927c0a41c1b8b3de5b745fe37d475e4f
JuliaMoller <juliadmoller@gmail.com>
2021-03-28 17:14:57 +0100

    cleaning old test code (to transform object into img)

--- resources/js/print.js
+++ resources/js/print.js
@@ -1,30 +1,5 @@
 function printMonster (){
-    // transform all objects inside the div to img
-    // var objs =  document.querySelectorAll("#monsterCanvas object");
-    // var monsterBg = document.getElementById("monsterCanvas");
-    // var monsterDiv = document.createElement("div");
-    // monsterDiv.setAttribute("class", "parent");
-   // monsterDiv.style.backgroundColor = monsterBg.style.backgroundColor;
-
-//     for (var i = 0; i < objs.length; i++) {
-//         var data = objs[i].getAttribute("data");
-//         var className = objs[i].getAttribute("class");
-//         var style = objs[i].getAttribute("style");
-
-//         var img = document.createElement("img");
-//         img.src = data;
-//         img.setAttribute("class", className);
-//         img.setAttribute("style", style);
-    
-//         console.log(img);
-    
-//         monsterDiv.appendChild(img);
-//     }
-
-//    document.body.appendChild(monsterDiv);
-
     // dom to image
-
     setTimeout(() => {
         domtoimage.toPng(monsterCanvas)
         .then(function (dataUrl) {
